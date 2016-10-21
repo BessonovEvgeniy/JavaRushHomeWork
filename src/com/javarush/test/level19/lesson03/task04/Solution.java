0c82dbdc417c77cf695c2a38570c8fd20bdde4a4
Evgeniy Bessonov <Bessonov.Evgeniy@gmail.com>
2016-10-21 14:28:56 +0300

    level19.03.04 is done!

--- src/com/javarush/test/level19/lesson03/task04/Solution.java
+++ src/com/javarush/test/level19/lesson03/task04/Solution.java
@@ -1,6 +1,9 @@
 package com.javarush.test.level19.lesson03.task04;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 /* И еще один адаптер
@@ -14,19 +17,32 @@ import java.util.Scanner;
 
 public class Solution {
     public static class PersonScannerAdapter implements PersonScanner {
-        private Scanner scanner;
+        Scanner scanner;
+
         PersonScannerAdapter(Scanner scanner){
             this.scanner = scanner;
         }
 
         @Override
         public Person read() throws IOException {
-            String[] fields = this.scanner.;
-            return new Person();
+            String line = scanner.nextLine();
+            String[] params = line.split(" ",4);
+            SimpleDateFormat formatter = new SimpleDateFormat("dd MM yyyy");
+            Date birthDate = new Date();
+            try {
+                birthDate = formatter.parse(params[3]);
+            }
+            catch (ParseException e){}
+            return new Person(params[1], params[2], params[0], birthDate);
         }
+
         @Override
         public void close() throws IOException {
             scanner.close();
         }
+    }
+    public static void main(String[] args) throws Exception {
+        PersonScannerAdapter psa = new PersonScannerAdapter(new Scanner("Иванов Иван Иванович 31 12 1950"));
+        Person person = psa.read();
     }
 }
